3bf1ef9a0d08afe578900d64fcb6ce23962a00e2
Tom Lazore <tomlazore@gmail.com>
2022-12-21 01:52:07 +0000

    Saved progress on MethodParameters 2022-12-21 01:52:07 +0000

--- MethodParameters/src/main/java/Parrot.java
+++ MethodParameters/src/main/java/Parrot.java
@@ -12,6 +12,8 @@ public class Parrot {
      * @return text.
      */
     public String copy(String text){
-        return "return text please!";
+
+        String hello = text;
+        return hello;
     }
 }
