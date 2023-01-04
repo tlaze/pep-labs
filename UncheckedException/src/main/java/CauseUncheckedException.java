9440a120441b8effbce2547a30276cf0e0ec9373
Tom Lazore <tomlazore@gmail.com>
2023-01-04 05:01:37 +0000

    Saved progress on UncheckedException 2023-01-04 05:01:37 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -14,6 +14,7 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
+        int x = 2 / 0;
 
     }
 }
