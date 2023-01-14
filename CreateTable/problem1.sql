c306fa57c1ecaf688a45dd8e6fdb69fe267036b3
Tom Lazore <tomlazore@gmail.com>
2023-01-14 16:28:29 +0000

    Saved progress on CreateTable 2023-01-14 16:28:29 +0000

--- CreateTable/problem1.sql
+++ CreateTable/problem1.sql
@@ -1,4 +1,4 @@
-CREATE TABLE song (
-    title varchar(100),
-    song varchar(100)
+CREATE TABLE song (title varchar(100), artist varchar(100)
     );
+
+    insert into song (title ('Let it be'), artist ('Beatles'));
