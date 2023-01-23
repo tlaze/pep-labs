338c5b394c4680a193244a5cf45e8110c37290c7
Tom Lazore <tomlazore@gmail.com>
2023-01-23 00:59:12 +0000

    Saved progress on IndexOf 2023-01-23 00:59:12 +0000

--- IndexOf/src/main/java/IndexOf.java
+++ IndexOf/src/main/java/IndexOf.java
@@ -11,6 +11,13 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
-        return -1;
+        int index = -1;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == n){
+                index = i;
+            }
+        }
+        return index;
     }
 }
