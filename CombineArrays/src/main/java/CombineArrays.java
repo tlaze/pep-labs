7932290674099365d2fd4bb366138858dbaed3c2
Tom Lazore <tomlazore@gmail.com>
2023-02-01 05:46:54 +0000

    Saved progress on CombineArrays 2023-02-01 05:46:54 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -9,6 +9,16 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+        int[] arr3 = new int [arr1.length + arr2.length];
+
+        for(int i = 0; i < arr1.length; i++){
+            arr3[i] = arr1[i];
+        }
+
+        for(int j = 0; j < arr2.length; j++){
+            arr3[arr1.length + j] = arr2[j];
+
+        }
+        return arr3;
     }
 }
