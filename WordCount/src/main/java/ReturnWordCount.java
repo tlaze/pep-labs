4a9dcfe8fa384a23ef7a7e6a097c21b4c6fa7cf4
Tom Lazore <tomlazore@gmail.com>
2023-02-03 18:11:47 +0000

    Saved progress on WordCount 2023-02-03 18:11:47 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -6,13 +6,9 @@ public class ReturnWordCount {
      */
     public int count(String in){
 
-        int wordCount = 0;
+        int wordCount = in.split("\\s+").length;
 
-        for(int i = 0; i < in.length(); i++){
-            if(in.charAt(i) == ' ' && in.charAt(i+1) != ' '){
-                wordCount++;
-            }
-        }
+        
         return wordCount;
     }
 }
