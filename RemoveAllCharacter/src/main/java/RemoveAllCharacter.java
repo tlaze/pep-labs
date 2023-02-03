055331a933206743c59244d6f418cfa0fd3dc94d
Tom Lazore <tomlazore@gmail.com>
2023-02-03 17:45:11 +0000

    Saved progress on RemoveAllCharacter 2023-02-03 17:45:11 +0000

--- RemoveAllCharacter/src/main/java/RemoveAllCharacter.java
+++ RemoveAllCharacter/src/main/java/RemoveAllCharacter.java
@@ -12,6 +12,7 @@ public class RemoveAllCharacter {
      * @return str, without any characters of type ch.
      */
     public String removeAll(String str, String ch){
-        return null;
+        String newStr = str.replaceAll(ch,"");
+        return newStr;
     }
 }
